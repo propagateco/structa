48327dfb071b0e28c108b698a54452d4007b0eba
imharrisonking <harrison.k@hotmail.co.uk>
2026-01-05 18:38:08 +0000

    Adds database, dns, email and secret infra

---
+++ infra/database.ts
@@ -0,0 +1,101 @@
+import { IS_DEPLOYED_STAGE, BRANCH_NAME } from "./dns";
+import { secret } from "./secret";
+
+// Create Neon provider with API key
+const neonProvider = new neon.Provider("NeonProvider", {
+    apiKey: secret.NeonApiKey.value,
+});
+
+const project =
+    $app.stage === "production"
+        ? new neon.Project(
+              "NeonProject",
+              {
+                  name: "propagate",
+              },
+              {
+                  provider: neonProvider,
+              },
+          )
+        : neon.Project.get(
+              "NeonProject",
+              secret.NeonProjectId.value,
+              undefined,
+              {
+                  provider: neonProvider,
+              },
+          );
+
+const branch =
+    $app.stage !== "production"
+        ? new neon.Branch(
+              "NeonBranch",
+              {
+                  name: BRANCH_NAME,
+                  projectId: project.id,
+                  parentId: project.defaultBranchId,
+              },
+              {
+                  provider: neonProvider,
+              },
+          )
+        : neon.Branch.get("NeonBranch", project.defaultBranchId, undefined, {
+              provider: neonProvider,
+          });
+
+const endpoint =
+    $app.stage !== "production"
+        ? new neon.Endpoint(
+              "NeonEndpoint",
+              {
+                  projectId: project.id,
+                  branchId: branch.id,
+              },
+              {
+                  provider: neonProvider,
+              },
+          )
+        : neon.Endpoint.get(
+              "NeonEndpoint",
+              project.defaultEndpointId,
+              undefined,
+              {
+                  provider: neonProvider,
+              },
+          );
+
+const role = new neon.Role(
+    "NeonRole",
+    {
+        name: "admin",
+        projectId: project.id,
+        branchId: branch.id,
+    },
+    {
+        provider: neonProvider,
+    },
+);
+
+const db = new neon.Database(
+    "NeonDatabase",
+    {
+        name: "structa-db",
+        projectId: project.id,
+        branchId: branch.id,
+        ownerName: role.name,
+    },
+    {
+        provider: neonProvider,
+    },
+);
+
+// Create a Linkable resource to use in your app
+export const database = new sst.Linkable("Database", {
+    properties: {
+        username: role.name,
+        password: role.password,
+        host: endpoint.host,
+        dbname: db.name,
+        url: $interpolate`postgresql://${role.name}:${role.password}@${endpoint.host}/${db.name}?sslmode=require`,
+    },
+});
--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,30 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "monorepo-template",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    const storage = await import("./infra/storage");
-    await import("./infra/api");
+    app(input) {
+        return {
+            name: "structa",
+            removal: input?.stage === "production" ? "retain" : "remove",
+            protect: ["production"].includes(input?.stage),
+            home: "aws",
+            providers: {
+                aws: {
+                    profile:
+                        input.stage === "production"
+                            ? "structa-production"
+                            : "structa-dev",
+                    region: "us-east-1",
+                },
+                neon: "0.9.0",
+            },
+        };
+    },
+    async run() {
+        const storage = await import("./infra/storage");
+        await import("./infra/api");
 
-    return {
-      MyBucket: storage.bucket.name,
-    };
-  },
+        return {
+            MyBucket: storage.bucket.name,
+        };
+    },
 });
