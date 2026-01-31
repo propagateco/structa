f4ac3fd9883a7218bbebb3ce174435ac3c32db8c
imharrisonking <hi@harrisonk.ing>
2026-01-31 18:02:08 +0000

    Updates input active state

--- packages/web/src/components/ui/input.tsx
+++ packages/web/src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             <input
                 type={type}
                 className={cn(
-                    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-colors duration-200 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-ds-mono-400 focus-visible:ring-2 focus-visible:ring-ring -focus-visible:ring-offset-1 hover:border-ds-mono-500 disabled:cursor-not-allowed disabled:opacity-50',
+                    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-colors duration-200 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-accent/50 -focus-visible:ring-offset-1 hover:border-ds-mono-500 disabled:cursor-not-allowed disabled:opacity-50',
                     className
                 )}
                 ref={ref}
