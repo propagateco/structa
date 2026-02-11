1d19cd50e017cddce8334e1a701e948c907843e7
imharrisonking <hi@harrisonk.ing>
2026-02-11 12:59:23 +0000

    Fixes z index for Footer and changes wordmark-footer-light.webp

--- packages/web/src/routes/_marketing.tsx
+++ packages/web/src/routes/_marketing.tsx
@@ -12,7 +12,7 @@ function LayoutComponent() {
             <Header />
 
             {/* Main content wrapper */}
-            <div className="flex-1 relative">
+            <div className="flex-1 relative z-10">
                 {/* Side gutters with noise texture */}
                 <div className="absolute left-0 top-0 bottom-0 w-3 sm:w-4 md:w-8 pointer-events-none bg-ds-mono-100 dark:bg-background border-b border-ds-powder/50 dark:border-ds-powder/[0.08]">
                     <div
@@ -30,8 +30,6 @@ function LayoutComponent() {
                     <Outlet />
                 </main>
             </div>
-
-            {/* Footer - outside main, inside the flex container */}
             <Footer />
         </div>
     );
