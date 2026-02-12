114d63fd95185a9f1ff5c78597b38a3cd63d56df
imharrisonking <hi@harrisonk.ing>
2026-02-12 18:11:18 +0000

    Updated files

--- packages/web/.content-collections/generated/allPosts.js
+++ packages/web/.content-collections/generated/allPosts.js
@@ -4,7 +4,7 @@ export default [
     "title": "12-Week Renovation Checklist",
     "slug": "renovation-checklist",
     "description": "A comprehensive week-by-week guide to planning your home renovation project, from initial budgeting to final walkthrough.",
-    "author": "Structa Team",
+    "author": "Hester @lifewithcharacter",
     "publishedAt": "2026-01-31",
     "readTime": "25 min read",
     "previewPercentage": 30,
